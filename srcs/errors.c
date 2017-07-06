4bf9c35b98658bfe293293402ccc7427a878f592
Syberam <sbonnefo@student.42.fr>
2017-07-06 23:54:58 +0200

    put env in lst

---
+++ srcs/errors.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbonnefo <sbonnefo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/23 00:23:42 by sbonnefo          #+#    #+#             */
+/*   Updated: 2017/07/06 20:19:32 by sbonnefo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minish_errors.h"
+
+void		error_wgcmd(char *av)
+{
+	ft_putstr_fd("MiniShell: Command not found: ", 2);
+	ft_putendl_fd(av, 2);
+	exit (0);
+}
