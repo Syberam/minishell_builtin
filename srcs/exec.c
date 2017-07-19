76fef5d478be42ac0f6163b03abd5d5dd2d18102
Syberam <sbonnefo@student.42.fr>
2017-07-19 03:53:36 +0200

    corrected env len

--- srcs/exec.c
+++ srcs/exec.c
@@ -6,7 +6,7 @@
 /*   By: sbonnefo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/17 23:16:23 by sbonnefo          #+#    #+#             */
-/*   Updated: 2017/07/19 03:11:47 by sbonnefo         ###   ########.fr       */
+/*   Updated: 2017/07/19 03:52:18 by sbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char			**env_to_strtab(t_env *env)
 	len = ft_envlen(env);
 	envtab = (char **)ft_memalloc(sizeof(char *) * (len + 1));
 	i = 0;
-	while ((size_t)i < len && env)
+	while (env)
 	{
 		envtab[i] = env->var;
 		i++;
